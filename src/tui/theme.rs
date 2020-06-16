694d51b78a75a185a721d04c5d7195dec6a44f08
Andrew Jensen <andrewjensen90@gmail.com>
2020-06-16 16:40:50 -0600

    Set background color to Black too

--- src/tui/theme.rs
+++ src/tui/theme.rs
@@ -4,7 +4,7 @@ use cursive::Cursive;
 pub fn create_theme(siv: &Cursive) -> Theme {
     let mut theme = siv.current_theme().clone();
 
-    // theme.palette[PaletteColor::View] = Color::TerminalDefault;
+    theme.palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
     theme.palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
     theme.palette[PaletteColor::Primary] = Color::Dark(BaseColor::White);
     theme.palette[PaletteColor::Secondary] = Color::Light(BaseColor::Red);
